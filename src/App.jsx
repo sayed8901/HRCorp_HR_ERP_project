588efa0841b1f1b4ff3ab7b4858f4bd70d77ac4a
sayed8901 <sayed91515@gmail.com>
2025-01-19 21:42:50 +0600

    code reviewed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
-// import Navbar from "./navbar/components/Navbar/Navbar";
 import { useContext } from "react";
 import { ThemeContext } from "./contextProviders/ThemeContextProvider";
 import Navbar from "./components/Navbar/Navbar";
